7e25332649681a9353c1efa5571958e9cc5685c7
Grey280 <grey280@grey280.net>
2016-04-08 19:06:50 -0700

    Add user data
    User data now includes emails, with validation, and passwords, hashed
    via crypt.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.4'
 gem 'bootstrap-sass', '3.2.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+# Use bcrypt to keep things nice and secure
+gem 'bcrypt', '3.1.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409013416) do
+ActiveRecord::Schema.define(version: 20160409020020) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -33,8 +33,11 @@ ActiveRecord::Schema.define(version: 20160409013416) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
